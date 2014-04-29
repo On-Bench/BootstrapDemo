f9ed6cf166ed5aed89cb7e5762558586bf77e997
Đinh Duy Linh <linhduydinh@gmail.com>
2014-04-29 10:12:27 +0700

    Updated One
    Demo Bootstrap

---
+++ BootstrapDemo/Scripts/JavaScript.js
@@ -0,0 +1,27 @@
+﻿$('.img').contenthover({
+    overlay_background: '#000',
+    overlay_opacity: 0.8
+});
+
+
+
+$('.mylightbox').click(function () {
+    $('.demoLightbox').addClass('in');
+    $('.demoLightbox').removeClass('hide');
+});
+
+$('html').click(function () {
+    $('.demoLightbox').hide();
+    $(".modal-backdrop.in").remove();
+    $('.demoLightbox').removeClass('in');
+});
+
+
+$("[data-toggle='tooltip']").click(function () {
+    $('.myAlert').removeClass('hide')
+});
+
+$('[data-toggle="tooltip"]').tooltip();
+
+$("[data-toggle='popover']").popover();
+
---
+++ BootstrapDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BootstrapDemo.Startup))]
+namespace BootstrapDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
